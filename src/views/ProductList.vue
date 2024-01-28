2780160c5af89a198e8dac9ab3fcabf91ced13f7
Lianum Frada Bahesna <fbahezna@gmail.com>
2024-01-28 17:51:11 +0700

    feature: add product lists

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -6,32 +6,13 @@
       </ion-card-header>
       <ion-card-content>
         <ion-list>
-          <ion-item>
+          <ion-item v-for="(product, index) in products.data" :key="index">
             <ion-thumbnail slot="start">
               <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
             </ion-thumbnail>
-            <ion-label>Miranda</ion-label>
-          </ion-item>
-
-          <ion-item>
-            <ion-thumbnail slot="start">
-              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-            </ion-thumbnail>
-            <ion-label>Obat</ion-label>
-          </ion-item>
-
-          <ion-item>
-            <ion-thumbnail slot="start">
-              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-            </ion-thumbnail>
-            <ion-label></ion-label>
-          </ion-item>
-
-          <ion-item lines="none">
-            <ion-thumbnail slot="start">
-              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/thumbnail.svg" />
-            </ion-thumbnail>
-            <ion-label>Item</ion-label>
+            <ion-label>{{product.Name}}</ion-label>
+            <ion-label>{{product.Price}}</ion-label>
+            <ion-label>{{product.Qty}}</ion-label>
           </ion-item>
         </ion-list>
       </ion-card-content>
@@ -41,12 +22,32 @@
 </template>
 
 <script lang="ts">
-import { IonPage, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {IonCard, IonCardContent, IonCardTitle, IonPage} from '@ionic/vue';
+import {defineComponent, ref} from 'vue';
+// const URL = process.env.BASE_URL_V1
+const headers = { "Content-Type": "application/json" };
 
 export default defineComponent({
   name: 'ProductList',
-  components: { IonPage, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle },
+  components: { IonPage, IonCard, IonCardContent, IonCardTitle },
+  setup(){
+    const products = ref([])
+      try {
+        const response = fetch('http://localhost:8080/v1/products', { headers });
+        response.then((res)=> {
+          if(!res.ok){
+              throw new Error(`HTTP error! Status: ${res.status}`);
+          }
+          return res.json()
+        }).then((data) => {
+          products.value = data
+        })
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+
+    return {products}
+  }
 });
 </script>
 
